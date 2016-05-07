4c8b85e5123c9c9fc113e93fdf6189662adcbd83
Santos Gallegos <santos_g@outlook.com>
2016-05-07 18:29:52 -0500

    Implementada aulas.h

---
+++ aulas.cpp
@@ -0,0 +1,55 @@
+#include "aulas.h"
+#include "log.h"
+
+#include <regex>
+
+void Aulas::set_aulas_file(std::string path)
+{
+    aulas_csv.set_file(path);
+}
+
+void Aulas::set_logfile(Log* log)
+{
+    errorlog = log;
+}
+
+std::vector<Aula> Aulas::get_aulas()
+{
+    std::vector<Aula> aulas;
+    
+    aulas_csv.init();
+    std::string line;
+    std::vector<std::string> record;
+    while ((line = aulas_csv.next_record()) != "") {
+        record = parser_record(line);
+        if (!record.empty()) {
+            Aula aula;
+            aula.codigo = record[0];
+            aula.capacidad = atoi(record[1].c_str());
+            aulas.push_back(aula);
+        } else {
+            errorlog->write("Aula no valida. Data: " + line);
+        }
+    }
+    aulas_csv.close();
+    
+    return aulas;
+}
+
+
+std::vector<std::string> Aulas::parser_record(std::string record)
+{
+    std::vector<std::string> records;
+    std::regex regex_docente("^(\\w\\d+),(\\d+)$");
+    std::smatch groups;
+    
+    if (!std::regex_match(record, groups, regex_docente))
+       return records;
+    
+    for (int i = 1; i < 3; i++)
+        records.push_back(groups[i]);
+    return records;
+}
+
+
+
---
+++ aulas.h
@@ -0,0 +1,23 @@
+#ifndef AULAS_H
+#define AULAS_H
+
+#include "csvfile.h"
+#include "log.h"
+#include <vector>
+
+
+struct Aula {
+    std::string codigo;
+    int capacidad;
+};
+
+struct Aulas {
+    Log* errorlog;
+    Csvfile aulas_csv;
+    void set_aulas_file(std::string path);
+    void set_logfile(Log* log);
+    std::vector<Aula> get_aulas();
+    std::vector<std::string> parser_record(std::string record);
+};
+
+#endif /* AULA_H */
